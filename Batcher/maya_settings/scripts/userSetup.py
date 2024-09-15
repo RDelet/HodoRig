efa09e111dc4120a73f5ceb029433ae4acef8c5b
Rémi Deletrain <remi.deletrain@gmail.com>
2024-09-15 18:43:53 +0200

    Updata maya batch

--- Batcher/maya_settings/scripts/userSetup.py
+++ Batcher/maya_settings/scripts/userSetup.py
@@ -3,7 +3,7 @@ import sys
 
 # Init current module
 dir, _ = os.path.split(__file__)
-module_dir = os.path.normpath(os.path.join(dir, "../"))
+module_dir = os.path.normpath(os.path.join(dir, "../.."))
 sys.path.insert(0, module_dir)
 
 import HodoRig
--- Batcher/maya_standAlone.py
+++ Batcher/maya_standAlone.py
@@ -2,14 +2,9 @@ import os
 
 from maya import standalone
 
-# Set maya settings directory
 dir, _ = os.path.split(__file__)
-maya_settings_dir = os.path.normpath(os.path.join(dir, "batch_settings"))
+maya_settings_dir = os.path.normpath(os.path.join(dir, "maya_settings"))
 os.environ["MAYA_APP_DIR"] = maya_settings_dir
 os.environ["MAYA_ENV_DIR"] = maya_settings_dir
-"""
-if "MAYA_APP_DIR" in os.environ:
-    del os.environ["MAYA_APP_DIR"]
-"""
 
 standalone.initialize()
